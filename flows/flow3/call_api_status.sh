b3f7116a34b6a26b632788518a0728e95dfcdf3c
Lucien van Wouw <lwo@iisg.nl>
2015-09-12 14:11:20 +0200

    Add message to log and mail it.

--- flows/flow3/call_api_status.sh
+++ flows/flow3/call_api_status.sh
@@ -96,6 +96,7 @@ function exit_error() {
     message=$3
     call_api_status "$pid" "$status" true "$message"
 
+    echo $message >> $log
     /usr/bin/sendmail --body "$log" --from "$flow_client" --to "$flow_notificationEMail" --subject "Error report for $archiveID" --mail_relay "$mail_relay" --mail_user "$mail_user" --mail_password "$mail_password" >> $log
 
     exit 1
