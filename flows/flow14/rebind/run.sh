375d3df9507ac5c8932de84ce7309421ac391b75
lwo <lwo@iisg.nl>
2016-04-29 16:50:51 +0200

    Rebind EAD procedure.

--- flows/flow14/rebind/run.sh
+++ flows/flow14/rebind/run.sh
@@ -33,9 +33,117 @@ fi
 # Pull out the unitid values. Create a concordance list as used by the PID bind procedure.
 #-----------------------------------------------------------------------------------------------------------------------
 file_concordancetable="${work}/${archiveID}"
-python create_concordancetable.py --xml_file="$file" --xsl_file="create_concordancetable.xsl" --result_file="$file_concordancetable" >> $log
+python ${DIGCOLPROC_HOME}/util/xslt_transformer.py --xml_file="$file" --xsl_file="create_concordancetable.xsl" --result_file="$file_concordancetable" >> $log
+rc=$?
+if [[ $rc != 0 ]] ; then
+    echo "xslt_transformer.py threw an error ${rc}">>$log
+    exit 1
+fi
+if [ ! -f $file_concordancetable ]
+then
+    echo "Unable to create ${file_concordancetable}">>$log
+    exit 1
+fi
 
 
+#-----------------------------------------------------------------------------------------------------------------------
+# For each unitid in the csv, append the second item.
+#-----------------------------------------------------------------------------------------------------------------------
+while read line
+do
+    IFS=, read Objectnummer Inventarisnummer <<< "$line"
+    mets_item_2="${or}/mets/10622/${na}/${archiveID}.${Inventarisnummer}/2" # e.g. http://disseminate.objectrepository.org/mets/10622/ARCH00720.1/2
+    file_item_2="${work}/${archiveID}.${Inventarisnummer}.xml"
+    wget -O "$file_item_2" "$mets_item_2"
+
+    pid=$(python ${DIGCOLPROC_HOME}/util/xslt_transformer.py --xml_file="$mets_item_2" --xsl_file="get_item_pid.xsl" --result_file="$file_concordancetable")
+    rc=$?
+    if [[ $rc != 0 ]]
+    then
+        echo "Could not get a PID for Inventarisnummer ${Inventarisnummer}. Error ${rc}">>$log
+        continue
+    fi
+    if [ -z "$pid" ]
+    then
+        echo "Could not get a PID for Inventarisnummer ${Inventarisnummer}.">>$log
+        continue
+    fi
+
+    objid="${na}/${archiveID}.${Inventarisnummer}"
+    soapenv="<?xml version='1.0' encoding='UTF-8'?>  \
+        <soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:pid='http://pid.socialhistoryservices.org/'>  \
+            <soapenv:Body> \
+                <pid:UpsertPidRequest> \
+                    <pid:na>$na</pid:na> \
+                    <pid:handle> \
+                        <pid:pid>$objid</pid:pid> \
+                        <pid:locAtt> \
+                                <pid:location weight='1' href='$catalog/$archiveID/ArchiveContentList#$Inventarisnummer'/> \
+                                <pid:location weight='0' href='$catalog/$archiveID/ArchiveContentList#$Inventarisnummer' view='catalog'/> \
+                                <pid:location weight='0' href='$or/mets/$objid' view='mets'/> \
+                                <pid:location weight='0' href='$or/pdf/$objid' view='pdf'/> \
+                                <pid:location weight='0' href='$or/file/master/$pid' view='master'/> \
+                                <pid:location weight='0' href='$or/file/level1/$pid' view='level1'/> \
+                                <pid:location weight='0' href='$or/file/level2/$pid' view='level2'/> \
+                                <pid:location weight='0' href='$or/file/level3/$pid' view='level3'/> \
+                            </pid:locAtt> \
+                    </pid:handle> \
+                </pid:UpsertPidRequest> \
+            </soapenv:Body> \
+        </soapenv:Envelope>"
+
+
+    echo "Sending $objid" >> $log
+    wget -O /dev/null --header="Content-Type: text/xml" \
+        --header="Authorization: bearer $pidwebserviceKey" --post-data "$soapenv" \
+        --no-check-certificate $pidwebserviceEndpoint
+
+    rc=$?
+    if [[ $rc != 0 ]]; then
+        echo "Error from PID webservice: $rc">>$log
+        echo $soapenv >> $log
+        cat $file >> $log
+    fi
+
+    # The main archival ID
+	# This will bind to the catalog as well.
+	pid=$na/$archiveID
+	soapenv="<?xml version='1.0' encoding='UTF-8'?>  \
+		<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:pid='http://pid.socialhistoryservices.org/'>  \
+			<soapenv:Body> \
+				<pid:UpsertPidRequest> \
+					<pid:na>$na</pid:na> \
+					<pid:handle> \
+						<pid:pid>$pid</pid:pid> \
+						<pid:locAtt> \
+								<pid:location weight='1' href='$catalog/$archiveID'/> \
+								<pid:location weight='0' href='$catalog/$archiveID' view='catalog'/> \
+								<pid:location weight='0' href='$oai?verb=GetRecord&amp;identifier=oai:socialhistoryservices.org:$na/$archiveID&amp;metadataPrefix=ead' view='ead'/> \
+								<pid:location weight='0' href='$or/file/master/$pid' view='master'/> \
+								<pid:location weight='0' href='$or/file/level1/$pid' view='level1'/> \
+								<pid:location weight='0' href='$or/file/level2/$pid' view='level2'/> \
+								<pid:location weight='0' href='$or/file/level3/$pid' view='level3'/> \
+							</pid:locAtt> \
+					</pid:handle> \
+				</pid:UpsertPidRequest> \
+			</soapenv:Body> \
+		</soapenv:Envelope>"
+
+	echo "Sending $pid" >> $log
+	wget -O /dev/null --header="Content-Type: text/xml" \
+        --header="Authorization: bearer $pidwebserviceKey" --post-data "$soapenv" \
+        --no-check-certificate $pidwebserviceEndpoint
+
+    rc=$?
+	if [[ $rc != 0 ]]; then
+		echo "Error from PID webservice: $rc">>$log
+		echo $soapenv >> $log
+	fi
+
+done < $file_concordancetable
+
+
+echo 
 
 exit 0
 
