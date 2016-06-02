cc99591ccc961263aac86a5e42e7b17a10946573
lwo <lwo@iisg.nl>
2016-06-02 13:09:17 +0200

    Add the valid ' minimal' access status

--- util/oai2harvester.groovy
+++ util/oai2harvester.groovy
@@ -17,7 +17,7 @@ class Oai2Harvester {
     private def required = ['na', 'baseURL', 'verb']
     private static String ACCESS_DEFAULT = 'closed'
     private static int PAUSE_SECONDS = 10
-    private static access_stati = ['open', 'restricted', 'closed', 'irsh', 'pictoright']
+    private static access_stati = ['closed', 'irsh', 'minimal', 'open', 'pictoright', 'restricted']
 
     public Oai2Harvester(def args) {
         orAttributes = args
