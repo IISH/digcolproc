0db230fda0b1852f1f6d12e3155e044fba75d763
lwo <lwo@iisg.nl>
2016-07-20 13:42:19 +0200

    Add a script to create a report

---
+++ flows/utils/report_image_quality/run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# run.sh
+#
+# Usage:
+# run.sh [na] [folder name]
+#
+# Description:
+# Make a report based on a supplied CSV table: report-data-csv
+# The csv must be of the form: "TCN Value","Call Number Label","Barcode"
+# The report will create a new CSV with an extra column, containing the content information about the file.
+#
+# Jira: https://jira.socialhistoryservices.org/browse/DIGCOLPROC-485
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+# load environment variables
+#-----------------------------------------------------------------------------------------------------------------------
+source "${DIGCOLPROC_HOME}setup.sh" $0 "$@"
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+# Create the report
+#-----------------------------------------------------------------------------------------------------------------------
+file="${fileSet}/report-data.csv"
+if [ -f "$file" ]
+then
+    echo "File not found: ${file}"
+    exit 1
+else
+    python file_content.py --file "$file" --na 10622 --url "${or}" >> $log
+fi
+
+
+rc=$?
+if [[ $rc == 0 ]]
+then
+    echo "I think we are done for today..."
+    exit 0
+else
+    echo "The procedure threw an error code ${rc}"
+    exit 1
+fi
