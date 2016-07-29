9d59ca1d7029a6f20e2f20b9d8ea4214e9fbaa6d
Gordan Cupac <gcu@iisg.nl>
2016-07-29 12:44:24 +0200

    added debug info

--- flows/flow3/api_call/run.sh
+++ flows/flow3/api_call/run.sh
@@ -37,6 +37,7 @@ fi
 # Create a folder for each PID we find.
 #-----------------------------------------------------------------------------------------------------------------------
 function call_api_folders {
+    echo -e "\n\nfunction: call_api_folders" | tee -a $log # debug info
 
     for na in $flow3_hotfolders*
     do
@@ -48,8 +49,10 @@ function call_api_folders {
 
             # get all pids with a status NEW_DIGITAL_MATERIAL_COLLECTION
             request="curl --insecure ${acquisition_database}/service/folders?access_token=${acquisition_database_access_token} | jq .pids[]"
-            echo "request=${request}">>$log
+            echo "request: ${request}" | tee -a $log # debug info
+
             pids=$(eval ${request})
+            echo "Returned pids: ${pids}" | tee -a $log # debug info
 
             # Create a folder for each PID
             for pid in ${pids}
@@ -59,14 +62,15 @@ function call_api_folders {
                 pid="${pid#\"}"
                 id=$(basename $pid) # And remove the prefix so we get BULK12345
 
-                echo "\$pid = $pid">>$log
-                echo "\$id = $id">>$log
+                echo "\$pid = $pid" | tee -a $log # debug info
+                echo "\$id = $id" | tee -a $log # debug info
 
                 # Tell what we are doing
                 call_api_status $pid $FOLDER $RUNNING
 
                 # Create a folder for the PID
                 folder=$offloader/$id
+
                 # check if the directory exists
                 if [ -d "$folder" ]; then
                     # TODO: Question: is it necessary to send a failure and the message 'folder already exists'? You cannot go to the next status!
@@ -117,11 +121,14 @@ function call_api_folders {
 # Create a backup.txt event for each PID we find.
 #-----------------------------------------------------------------------------------------------------------------------
 function call_api_backup() {
+    echo -e "\n\nfunction: call_api_backup" | tee -a $log # debug info
 
     # Get all the PIDs with a status MATERIAL_UPLOADED
     request="curl --insecure ${acquisition_database}/service/startBackup?access_token=${acquisition_database_access_token} | jq .pids[]"
-    echo "request=${request}">>$log
+    echo "request: ${request}" | tee -a $log # debug info
+
     pids=$(eval ${request})
+    echo "Returned pids: ${pids}" | tee -a $log # debug info
 
     for pid in ${pids}
     do
@@ -129,7 +136,12 @@ function call_api_backup() {
         pid="${pid%\"}"
         pid="${pid#\"}"
         id=$(basename $pid) # And remove the prefix
+
+        echo "\$pid = $pid" | tee -a $log # debug info
+        echo "\$id = $id" | tee -a $log # debug info
+
         if [[ $? == 0 ]] ; then
+            echo "${DIGCOLPROC_HOME}util/place_event.sh flow3 backup.txt ${id}" | tee -a $log # debug info
             "${DIGCOLPROC_HOME}util/place_event.sh" flow3 backup.txt $id
         fi
     done
@@ -145,11 +157,14 @@ function call_api_backup() {
 # Create a backup.txt event for each PID we find.
 #-----------------------------------------------------------------------------------------------------------------------
 function call_api_restore() {
+    echo -e "\n\nfunction: call_api_restore" | tee -a $log # debug info
 
     # Get all the PIDs with a status READY_FOR_RESTORE
     request="curl --insecure ${acquisition_database}/service/startRestore?access_token=${acquisition_database_access_token} | jq .pids[]"
-    echo "request=${request}">>$log
+    echo "request: ${request}" | tee -a $log # debug info
+
     pids=$(eval ${request})
+    echo "Returned pids: ${pids}" | tee -a $log # debug info
 
     for pid in ${pids}
     do
@@ -157,7 +172,12 @@ function call_api_restore() {
         pid="${pid%\"}"
         pid="${pid#\"}"
         id=$(basename $pid) # And remove the prefix
+
+        echo "\$pid = $pid" | tee -a $log # debug info
+        echo "\$id = $id" | tee -a $log # debug info
+
         if [[ $? == 0 ]] ; then
+            echo "${DIGCOLPROC_HOME}util/place_event.sh flow3 restore.txt ${id}" | tee -a $log # debug info
             "${DIGCOLPROC_HOME}util/place_event.sh" flow3 restore.txt $id
         fi
     done
@@ -173,11 +193,14 @@ function call_api_restore() {
 # Create a backup.txt event for each PID we find.
 #-----------------------------------------------------------------------------------------------------------------------
 function call_api_ingest() {
+    echo -e "\n\nfunction: call_api_ingest" | tee -a $log # debug info
 
     # Get all the PIDs with a status READY_FOR_PERMANENT_STORAGE
     request="curl --insecure $acquisition_database/service/startIngest?access_token=${acquisition_database_access_token} | jq .pids[]"
-    echo "request=${request}">>$log
+    echo "request: ${request}" | tee -a $log # debug info
+
     pids=$(eval ${request})
+    echo "Returned pids: ${pids}" | tee -a $log # debug info
 
     for pid in ${pids}
     do
@@ -185,7 +208,12 @@ function call_api_ingest() {
         pid="${pid%\"}"
         pid="${pid#\"}"
         id=$(basename $pid) # And remove the prefix
+
+        echo "\$pid = $pid" | tee -a $log # debug info
+        echo "\$id = $id" | tee -a $log # debug info
+
         if [[ $? == 0 ]] ; then
+            echo "${DIGCOLPROC_HOME}util/place_event.sh flow3 ingest.txt ${id}" | tee -a $log # debug info
             "${DIGCOLPROC_HOME}util/place_event.sh" flow3 ingest.txt $id
         fi
     done
@@ -200,6 +228,7 @@ function call_api_ingest() {
 # Run each api method
 #-----------------------------------------------------------------------------------------------------------------------
 function call_api(){
+    echo "Log file: ${log}" | tee -a $log # debug info
 
     call_api_folders
     call_api_backup
