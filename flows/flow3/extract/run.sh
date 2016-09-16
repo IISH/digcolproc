8a8b84573fed1999b7e73c2deff5df7df03ff70c
lwo <lwo@iisg.nl>
2016-09-16 17:49:07 +0200

    Add an extraction procedure.

--- flows/flow3/extract/run.sh
+++ flows/flow3/extract/run.sh
@@ -16,216 +16,63 @@
 source "${DIGCOLPROC_HOME}setup.sh" $0 "$@"
 source ../call_api_status.sh
 pid=$na/$archiveID
+TASK_ID=$EXTRACT
 
 
 
 #-----------------------------------------------------------------------------------------------------------------------
 # Commence job. Tell what we are doing
 #-----------------------------------------------------------------------------------------------------------------------
-call_api_status $pid $EXTRACT $RUNNING
+call_api_status $pid $TASK_ID $RUNNING
 
 
 
 #-----------------------------------------------------------------------------------------------------------------------
-# Make sure we do not have an instruction already there.
+# Get the manifest
 #-----------------------------------------------------------------------------------------------------------------------
-file_instruction=$fileSet/instruction.xml
-if [ -f "$file_instruction" ] ; then
-    exit_error "$pid" $STAGINGAREA "Instruction already present: ${file_instruction}. This may indicate the SIP is staged \
-    or the ingest is already in progress."
-fi
-
-
-
-#-----------------------------------------------------------------------------------------------------------------------
-# Lock the folder and it's contents
-#-----------------------------------------------------------------------------------------------------------------------
-chown -R root:root $fileSet
-
-
-
-#-----------------------------------------------------------------------------------------------------------------------
-# Produce a droid analysis. Remove the old profile if it is there.
-#-----------------------------------------------------------------------------------------------------------------------
-if [ -f $fileSet/manifest.csv ] ; then rm $fileSet/manifest.csv ; fi
-if [ -f $fileSet/manifest.xml ] ; then rm $fileSet/manifest.xml ; fi
-profile=$work/profile.droid
-droid --recurse -p $profile --profile-resources $fileSet>>$log
+manifest_file="$work/manifest.xml"
+manifest_url="${or}/mets/${pid}"
+wget -O "$manifest_file" --header="Authorization: bearer ${pidwebserviceKey}" --no-check-certificate "$manifest_url"
 rc=$?
-if [[ $rc != 0 ]] ; then
-    exit_error "$pid" $STAGINGAREA "Droid profiling threw an error."
-fi
-
-
-
-#-----------------------------------------------------------------------------------------------------------------------
-# produce a report.
-#-----------------------------------------------------------------------------------------------------------------------
-profile_csv=$profile.csv
-droid -p $profile --export-file $profile_csv >> $log
-rc=$?
-if [[ $rc != 0 ]] ; then
-    exit_error "$pid" $STAGINGAREA "Droid reporting threw an error."
-fi
-if [ ! -f $profile_csv ] ; then
-	exit_error "$pid" $STAGINGAREA "Unable to create a droid profile: ${profile_csv}"
-fi
-
-
-
-#-----------------------------------------------------------------------------------------------------------------------
-# Now extend the report with two columns: a md5 checksum and a persistent identifier
-#-----------------------------------------------------------------------------------------------------------------------
-profile_extended_csv=$profile.extended.csv
-python ${DIGCOLPROC_HOME}/util/droid_extend_csv.py --sourcefile $profile_csv --targetfile $profile_extended_csv --na $na --fileset $fileSet >> $log
-rc=$?
-if [[ $rc != 0 ]] ; then
-	exit_error "$pid" $STAGINGAREA "Got error ${rc}. Failed to extend the droid report with a PID and md5 checksum."
-fi
-
-
-
-#-----------------------------------------------------------------------------------------------------------------------
-# Create a mets document
-#-----------------------------------------------------------------------------------------------------------------------
-manifest=${fileSet}/manifest.xml
-python ${DIGCOLPROC_HOME}/util/droid_to_mets.py --sourcefile $profile_extended_csv --targetfile $manifest --objid "$pid"
-rc=$?
-if [[ $rc != 0 ]] ; then
-    exit_error "$pid" $STAGINGAREA "Failed to create a mets document."
-fi
-if [ ! -f $manifest ] ; then
-    exit_error "$pid" $STAGINGAREA "Failed to find a mets file at ${manifest}"
-fi
-
-
-
-#-----------------------------------------------------------------------------------------------------------------------
-# Add the mets file to the manifest.csv, in order for it to be in the xml instruction
-#-----------------------------------------------------------------------------------------------------------------------
-md5_hash=$(md5sum $manifest | cut -d ' ' -f 1)
-echo "\"\",\"1\",\"file:/${archiveID}/\",\"/${archiveID}/manifest.xml\",\"manifest.xml\",\"METHOD\",\"$STATUS\",\"SIZE\",\"File\",\"xml\",\"\",\"EXTENSION_MISMATCH\",\"${md5_hash}\",\"FORMAT_COUNT\",\"PUID\",\"application/xml\",\"Xml Document\",\"FORMAT_VERSION\",\"${pid}\",\"\"">>$profile_extended_csv
-
-
-
-#-----------------------------------------------------------------------------------------------------------------------
-# Do we have to make a package? If so ,we expect there is a marker.file.
-#-----------------------------------------------------------------------------------------------------------------------
-if [ -f "${work_base}/package.name" ]
+if [[ $rc != 0 ]] || [ ! -f "$manifest_file" ]
 then
-    source ../package.sh
-    pack
-    instruction
-    move_dir
-    work_instruction="dummy"
-else
-    #-----------------------------------------------------------------------------------------------------------------------
-    # Produce instruction from the report.
-    #-----------------------------------------------------------------------------------------------------------------------
-    work_instruction=$work/instruction.xml
-    python ${DIGCOLPROC_HOME}/util/droid_to_instruction.py -s $profile_extended_csv -t $work_instruction --objid "$pid" --access "$flow_access" --submission_date "$datestamp" --autoIngestValidInstruction "$flow_autoIngestValidInstruction" --deleteCompletedInstruction "$flow_deleteCompletedInstruction" --label "$archiveID $flow_client" --action "add" --notificationEMail "$flow_notificationEMail" --plan "InstructionPackage" >> $log
-    rc=$?
-    if [[ $rc != 0 ]] ; then
-        exit_error "$pid" $STAGINGAREA "Failed to create an instruction."
-    fi
-    if [ ! -f $work_instruction ] ; then
-        exit_error "$pid" $STAGINGAREA "Failed to find an instruction at ${file_instruction}"
-    fi
+    exit_error "$pid" $TASK_ID "Unable to download a file ${manifest_url}"
 fi
 
 
 
 #-----------------------------------------------------------------------------------------------------------------------
-# Now start the reverse mirror
+# For each file in the manifest, download the rar
 #-----------------------------------------------------------------------------------------------------------------------
-ftp_script_base=${work}/ftp.$archiveID.$datestamp
-ftp_script=${ftp_script_base}.files.txt
-bash ${DIGCOLPROC_HOME}util/ftp.sh "$ftp_script" "mirror --reverse --delete --verbose --exclude-glob *.md5 ${fileSet} /${archiveID}" "$flow_ftp_connection" "$log"
-rc=$?
-if [[ $rc != 0 ]] ; then
-    exit_error "$pid" $STAGINGAREA "FTP error with uploading the files."
-fi
-
-
-
-#-----------------------------------------------------------------------------------------------------------------------
-# Upload the instruction
-#-----------------------------------------------------------------------------------------------------------------------
-mv $work_instruction $file_instruction
-ftp_script=$ftp_script_base.instruction.txt
-bash ${DIGCOLPROC_HOME}util/ftp.sh "$ftp_script" "put -O /${archiveID} ${file_instruction}" "$flow_ftp_connection" "$log"
-rc=$?
-if [[ $rc != 0 ]] ; then
-    exit_error "$pid" $STAGINGAREA "FTP error with uploading the object repository instruction."
-fi
-
-
-#-----------------------------------------------------------------------------------------------------------------------
-# Bind the PID
-#-----------------------------------------------------------------------------------------------------------------------
-soapenv="<?xml version='1.0' encoding='UTF-8'?>  \
-		<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:pid='http://pid.socialhistoryservices.org/'>  \
-			<soapenv:Body> \
-				<pid:UpsertPidRequest> \
-					<pid:na>$na</pid:na> \
-					<pid:handle> \
-						<pid:pid>$pid</pid:pid> \
-						<pid:locAtt> \
-								<pid:location weight='1' href='$or/metadata/$pid'/> \
-								<pid:location weight='0' href='$or/file/master/$pid' view='master'/> \
-							</pid:locAtt> \
-					</pid:handle> \
-				</pid:UpsertPidRequest> \
-			</soapenv:Body> \
-		</soapenv:Envelope>"
-echo "Binding pid ${pid} with ${soapenv}" >> $log
-rc=$?
-wget -O /dev/null --header="Content-Type: text/xml" \
-    --header="Authorization: oauth $pidwebserviceKey" --post-data "$soapenv" \
-    --no-check-certificate $pidwebserviceEndpoint
-if [[ $rc != 0 ]] ; then
-    exit_error "$pid" $STAGINGAREA "The submission to the object repository succeeded. However we failed to bind the pid to the url of the manifest."
-fi
-
-
-#-----------------------------------------------------------------------------------------------------------------------
-# End job
-#-----------------------------------------------------------------------------------------------------------------------
-echo "Done. ALl went well at this side." >> $log
-call_api_status $pid $STAGINGAREA $FINISHED
-
-
-#-----------------------------------------------------------------------------------------------------------------------
-# Monitor instruction process
-#-----------------------------------------------------------------------------------------------------------------------
-echo "Waiting for instruction to be completely processed by the SOR." >> $log
-call_api_status $pid $SOR $REQUESTED
-running_confirmed=false
-while true
+manifest_urls="${work}/manifest_urls.txt"
+python ${DIGCOLPROC_HOME}/util/xslt_transformer.py --xml_file="$manifest_file" --xsl_file="urls.xsl" > "$manifest_urls"
+first_package=""
+while read line
 do
-    sor_status_code=$(python ${DIGCOLPROC_HOME}/util/instruction_status.py --pid "$pid" --token "$flow_access_token")
+    IFS=, read id title <<< "$line"
+    url="${or}/file/master/${id}"
+    file="${work}/${title}"
+    wget -O "$file" --header="Authorization: bearer ${pidwebserviceKey}" --no-check-certificate "$url"
     rc=$?
-
-    if [[ $rc == 0 ]] ; then
-        if [ "$running_confirmed" = false ] ; then
-            call_api_status $pid $SOR $RUNNING
-            running_confirmed=true
-        fi
-
-        if [[ $sor_status_code -eq 700 ]] ; then
-            exit_error "$pid" $SOR "There were problems processing the instruction."
-        elif [[ $sor_status_code -eq 900 ]] ; then
-            call_api_status $pid $SOR $FINISHED
-            break
-        fi
+    if [[ $rc != 0 ]] || [ ! -f "$file" ]
+    then
+        rm "${file}"
+        exit_error "$pid" $TASK_ID "Unable to download a file ${file}"
     fi
+done < "$manifest_urls"
 
-    sleep 15m
-done
 
 
 #-----------------------------------------------------------------------------------------------------------------------
-# TODO: Attempt to start the removal procedure automatically
+# Extract the package. Shoulld have a sequence of one in the name.
 #-----------------------------------------------------------------------------------------------------------------------
-#source ../remove/run.sh
+package="${work}/${archiveID}"
+unrar x "$package" "$fileSet" >> $log
+rc=$?
+if [[ $rc != 0 ]]
+then
+    exit_error "$pid" $TASK_ID "Unable to unpack ${package}"
+fi
+
+
 exit 0
